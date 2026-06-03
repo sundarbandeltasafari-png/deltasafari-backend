86e4cfa0b91b5f83bb2e9411c74f8db286c50a97
Kaushik Mahata <kaushikmahata5622gmail.com>
2026-06-03 09:19:13 +0530

    initial commit

--- Connection.js
+++ Connection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'u662254688_deltasafari',
     password: 'Admin1q2w--!',
     database: 'u662254688_deltasafari'
